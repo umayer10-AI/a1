437870c05a0d82b65e9beb3a285ed94cbabd4ee1
umayer10-AI <mdumayer10@gmail.com>
2026-04-15 20:22:21 +0600

    add

---
+++ src/app/star/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h2 className='text-center text-3xl font-bold my-25'>Hello Star</h2>
+        </div>
+    );
+};
+
+export default page;
